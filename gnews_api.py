de3f17205d779e7bd3575a768df0ba2273986eda
rasheedsaeed <dontusemyemailmate@gmail.com>
2020-04-20 04:00:47 +0430

    Added all features and ran black for formatting.

--- gnews_api.py
+++ gnews_api.py
@@ -1,12 +1,33 @@
 import requests
 import json
 
-class GNews: 
+
+class GNews:
     BASE_URL = "https://gnews.io/api/v3/"
+    COMMON_URL_PARAMS = "?lang={}?country={}?max={}?image={}?token={}?"
+    AVAILABLE_TOPICS = [
+        "world",
+        "nation",
+        "business",
+        "technology",
+        "entertainment",
+        "sports",
+        "science",
+        "health",
+    ]
+
     def __init__(
-            self, token: str, lang="en_UK", 
-            country="UK", mindate="None", 
-            maxdate="None"):
+        self,
+        token,
+        lang="en_UK",
+        country="UK",
+        mindate="None",
+        maxdate="None",
+        max_results=10,
+        image="Optional",
+        in_search="all",
+    ):
+
         token = token.strip()
         if not token:
             raise NameError("Gnews constructor: API (str) not defined!")
@@ -16,22 +37,73 @@ class GNews:
         self.country = country
         self.mindate = mindate
         self.maxdate = maxdate
+        self.max_results = max_results
+        self.image = image
+        self.in_search = in_search
 
-    def search(self, q: str) -> json:
-        # I need to handle the below. 
-        # If the user types "Any example" how do I handle the white space
-        # and how is it differentiate between the example and "         ".
-        # if not q:
-        #     raise NameError("Gnews search(): No query provided!")
+    def search(
+        self,
+        query: str,
+        lang=None,
+        country=None,
+        mindate=None,
+        maxdate=None,
+        max_results=None,
+        image=None,
+        in_search=None,
+    ) -> requests.models.Response:
 
-        response = requests.get(
-                GNews.BASE_URL + "search?q={0}?country={1}" +
-                "mindate={2}?maxdate={3}?lang={4}?token={5}".format(
-                        0="'" + q + "'", 1=self.country, 
-                        2=self.mindate, 3=self.maxdate, 
-                        4=self.lang, 5=self.token
-                )
+        if not query:
+            raise NameError("Gnews search(): No query provided!")
+
+        return requests.get(
+            GNews.BASE_URL
+            + "search"
+            + GNews.COMMON_URL_PARAMS
+            + "mindate={}?maxdate={}?in={}?q={}".format(
+                lang if lang else self.lang,
+                country if country else self.country,
+                max_results if max_results else self.max_results,
+                image if image else self.image,
+                self.token,
+                mindate if mindate else self.mindate,
+                maxdate if maxdate else self.maxdate,
+                in_search if in_search else self.in_search,
+                '"' + query + '"',
+            )
         )
 
-    def top_news(self) -> json:
-        return requests.get(GNews.BASE_URL + "top-news?token=%s" % self.token)
+    def top_news(
+        self, lang=None, country=None, max_results=None, image=None
+    ) -> requests.models.Response:
+
+        return requests.get(
+            GNews.BASE_URL
+            + "top-news"
+            + GNews.COMMON_URL_PARAMS.format(
+                lang if lang else self.lang,
+                country if country else self.country,
+                max_results if max_results else self.max_results,
+                image if image else self.image,
+                self.token,
+            )
+        )
+
+    def topic(
+        self, query: str, lang=None, country=None, max_results=None, image=None
+    ) -> requests.models.Response:
+
+        if query not in GNews.AVAILABLE_TOPICS:
+            raise NameError("Gnews topic(): the provided topic is not avaible!")
+
+        return requests.get(
+            GNews.BASE_URL
+            + query
+            + GNews.COMMON_URL_PARAMS.format(
+                lang if lang else self.lang,
+                country if country else self.country,
+                max_results if max_results else self.max_results,
+                image if image else self.image,
+                self.token,
+            )
+        )
