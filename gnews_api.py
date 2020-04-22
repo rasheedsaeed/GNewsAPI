b3ceadffcf92ae9e72c28e22a44f148238e47dac
rasheedsaeed <dontusemyemailmate@gmail.com>
2020-04-22 05:17:28 +0430

    Error handling added

--- gnews_api.py
+++ gnews_api.py
@@ -58,39 +58,63 @@ class GNews:
         if not query:
             raise NameError("Gnews search(): No query provided!")
 
-        return requests.get(
-            GNews.BASE_URL
-            + "search?"
-            + "&q={}&mindate={}&maxdate={}&in={}".format(
-                '"' + query + '"',
-                mindate if mindate else self.mindate,
-                maxdate if maxdate else self.maxdate,
-                in_search if in_search else self.in_search,
+        try:
+            response = requests.get(
+                GNews.BASE_URL
+                + "search?"
+                + "&q={}&mindate={}&maxdate={}&in={}".format(
+                    '"' + query + '"',
+                    mindate if mindate else self.mindate,
+                    maxdate if maxdate else self.maxdate,
+                    in_search if in_search else self.in_search,
+                )
+                + GNews.COMMON_URL_PARAMS.format(
+                    lang if lang else self.lang,
+                    country if country else self.country,
+                    max_results if max_results else self.max_results,
+                    image if image else self.image,
+                    self.token,
+                )
             )
-            + GNews.COMMON_URL_PARAMS.format(
-                lang if lang else self.lang,
-                country if country else self.country,
-                max_results if max_results else self.max_results,
-                image if image else self.image,
-                self.token,
-            )
-        )
+            response.raise_for_status()
+        except requests.exceptions.Timeout as error:
+            raise requests.exceptions.Timeout(error)
+        except requests.exceptions.ConnectionError as error:
+            raise requests.exceptions.ConnectionError(error)
+        except requests.exceptions.RequestException as error:
+            raise requests.exceptions.RequestException(error)
+        except requests.exceptions.HTTPError as error:
+            raise requests.exceptions.HTTPError(error)
+
+        return response
 
     def top_news(
         self, lang=None, country=None, max_results=None, image=None
     ) -> requests.models.Response:
 
-        return requests.get(
-            GNews.BASE_URL
-            + "top-news?"
-            + GNews.COMMON_URL_PARAMS.format(
-                lang if lang else self.lang,
-                country if country else self.country,
-                max_results if max_results else self.max_results,
-                image if image else self.image,
-                self.token,
+        try:
+            response = requests.get(
+                GNews.BASE_URL
+                + "top-news?"
+                + GNews.COMMON_URL_PARAMS.format(
+                    lang if lang else self.lang,
+                    country if country else self.country,
+                    max_results if max_results else self.max_results,
+                    image if image else self.image,
+                    self.token,
+                )
             )
-        )
+            response.raise_for_status()
+        except requests.exceptions.Timeout as error:
+            raise requests.exceptions.Timeout(error)
+        except requests.exceptions.ConnectionError as error:
+            raise requests.exceptions.ConnectionError(error)
+        except requests.exceptions.RequestException as error:
+            raise requests.exceptions.RequestException(error)
+        except requests.exceptions.HTTPError as error:
+            raise requests.exceptions.HTTPError(error)
+
+        return response
 
     def topic(
         self, query: str, lang=None, country=None, max_results=None, image=None
@@ -99,17 +123,28 @@ class GNews:
         if query.lower() not in GNews.AVAILABLE_TOPICS:
             raise NameError("Gnews topic(): the provided topic is not avaible!")
 
-        return requests.get(
-            GNews.BASE_URL
-            + "topics/"
-            + query
-            + "?"
-            + GNews.COMMON_URL_PARAMS.format(
-                lang if lang else self.lang,
-                country if country else self.country,
-                max_results if max_results else self.max_results,
-                image if image else self.image,
-                self.token,
+        try:
+            response = requests.get(
+                GNews.BASE_URL
+                + "topics/"
+                + query
+                + "?"
+                + GNews.COMMON_URL_PARAMS.format(
+                    lang if lang else self.lang,
+                    country if country else self.country,
+                    max_results if max_results else self.max_results,
+                    image if image else self.image,
+                    self.token,
+                )
             )
-        )
+            response.raise_for_status()
+        except requests.exceptions.Timeout as error:
+            raise requests.exceptions.Timeout(error)
+        except requests.exceptions.ConnectionError as error:
+            raise requests.exceptions.ConnectionError(error)
+        except requests.exceptions.RequestException as error:
+            raise requests.exceptions.RequestException(error)
+        except requests.exceptions.HTTPError as error:
+            raise requests.exceptions.HTTPError(error)
 
+        return response
